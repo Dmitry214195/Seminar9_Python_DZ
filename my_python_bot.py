99efcd7f92f444ea67fded495706ec2bee450589
Dmitry Artamonow <dmitri.artamonow@yandex.ru>
2022-12-26 17:41:36 +0500

    Создал Телеграмм бот

---
+++ my_python_bot.py
@@ -0,0 +1,24 @@
+from telegram import Bot
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+
+bot = Bot(token = '')
+updater = Updater(token = '')
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(update.effective_chat.id,"Отправте мне текст, и я удалю из текста все слова, содержащие 'абв'." )
+
+def delete_words(update, context):
+    string = update.message.text
+    context.bot.send_message(update.effective_chat.id, f"{' '.join(filter(lambda word: 'абв' not in word.lower(), string.split()))}")   
+
+
+start_handler = CommandHandler("start", start)
+message_handler = MessageHandler(Filters.text, delete_words)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(message_handler)
+
+
+updater.start_polling()
+updater.idle()
